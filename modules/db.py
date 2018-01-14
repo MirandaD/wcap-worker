f3543d6caeddb4460462cb86cc3f1a488044ebef
MirandaLi1020 <lisirui19881020@gmail.com>
2018-01-15 01:08:14 +1100

    add

--- modules/db.py
+++ modules/db.py
@@ -4,9 +4,9 @@ import os
 
 class DB():
     def __init__(self):
-        mongoConnectionStr = 'mongodb://localhost:27017/'
+        self.mongoConnectionStr = 'mongodb://localhost:27017/'
         if os.environ.get('APP_LOCATION') == 'heroku':
-            mongoConnectionStr = 'mongodb://mirandaLi:lisirui1020@cluster0-shard-00-00-eluxi.mongodb.net:27017,cluster0-shard-00-01-eluxi.mongodb.net:27017,cluster0-shard-00-02-eluxi.mongodb.net:27017/admin?readPreference=primary&ssl=true'
+            self.mongoConnectionStr = 'mongodb://mirandaLi:lisirui1020@cluster0-shard-00-00-eluxi.mongodb.net:27017,cluster0-shard-00-01-eluxi.mongodb.net:27017,cluster0-shard-00-02-eluxi.mongodb.net:27017/admin?readPreference=primary&ssl=true'
         self.client = MongoClient(self.mongoConnectionStr)
         self.db = self.client['wacp-dev']
         self.LoginInfo = self.db.loginInfo
