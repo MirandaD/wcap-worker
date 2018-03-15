4c5ddb8908edc7e72bd683aaaea6f6a20732671c
MirandaLi1020 <lisirui19881020@gmail.com>
2018-03-15 19:25:11 +1100

    fix

--- massge_consumer.py
+++ massge_consumer.py
@@ -32,18 +32,20 @@ class MassageConsumer():
                     #     print 'user drop out'
                     #     self.db.delete_login_info(loggedinUser)
                     # else:
-                        loginInfoId = loggedinUser['_id']
-                        loginInfo = loggedinUser
-                        userName = pydash.get(loginInfo, 'User.UserName')
-                        msg1,msg2 = self.massagers.get_msg(loginInfoId)
-                        if msg1 and len(msg1)>0:
-                            for singleMsg in msg1:
-                                print 'msg got'+msg1
-                                processRes = self.massagers.process_msg_slowly(singleMsg, loginInfo, responseMsg='Hi, thanks talking to Miranda\'s chatbot. Will get back to you ASAP.')
-                                # all the steps should return the response from http request, I shall decide how to handle failure.
-                                wechatCommunicateRet = pydash.get(processRes, 'BaseResponse.Ret', default=-1)
-                                if(wechatCommunicateRet != 0):
-                                    print 'Disconnected with wechat account', wechatCommunicateRet
+                    print 'process each'
+                    loginInfoId = loggedinUser['_id']
+                    loginInfo = loggedinUser
+                    userName = pydash.get(loginInfo, 'User.UserName')
+                    msg1,msg2 = self.massagers.get_msg(loginInfoId)
+                    if msg1 and len(msg1)>0:
+                        for singleMsg in msg1:
+                            print 'msg got'+msg1
+                            processRes = self.massagers.process_msg_slowly(singleMsg, loginInfo, responseMsg='Hi, thanks talking to Miranda\'s chatbot. Will get back to you ASAP.')
+                            # all the steps should return the response from http request, I shall decide how to handle failure.
+                            wechatCommunicateRet = pydash.get(processRes, 'BaseResponse.Ret', default=-1)
+                            print processRes
+                            if(wechatCommunicateRet != 0):
+                                print 'Disconnected with wechat account', wechatCommunicateRet
                 time.sleep(3)
                 
 
